be4e1dd7fbf9a7fc44056eff3f53f89ab04c300d
Todor Klasnakov <todor.klasnakov@scalefocus.com>
2020-12-22 21:05:23 +0200

    Remove imports

--- src/app/core/five-days-forecast/five-days-forecast.module.ts
+++ src/app/core/five-days-forecast/five-days-forecast.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { FiveDaysForecastComponent } from "./five-days-forecast.component";
 import { FiveDaysForecastRoutingModule } from "./five-days-forecast-routing.module";
 import { UiModule } from "../../ui/ui.module";
-import {DataService} from "../../shared/services/data/data.service";
 
 @NgModule({
     declarations: [ FiveDaysForecastComponent ],
--- src/app/core/weather/weather.module.ts
+++ src/app/core/weather/weather.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { UiModule } from "../../ui/ui.module";
 import { WeatherComponent } from "./weather.component";
-import { DataService } from "../../shared/services/data/data.service";
 
 @NgModule({
     declarations: [
         WeatherComponent
-    ],
-    providers: [
-        DataService
     ],
     imports: [
         HttpClientModule,
